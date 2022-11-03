1fe3a5a7047322ceac495e1875c8799289e58866
manisa <5manisa.panta@gmail.com>
2022-11-03 12:19:12 -0500

    archived wids kathmandu conference 2021

---
+++ agendaButton.js
@@ -0,0 +1,187 @@
+function onButtonClick0(){
+
+    color = document.getElementsByClassName("agenda-button-all");
+    color[0].style.background = "#e98300";
+
+    color = document.getElementsByClassName("agenda-button-session1");
+    color[0].style.background = "#53284f";
+
+    color = document.getElementsByClassName("agenda-button-session2");
+    color[0].style.background = "#53284f";
+
+    color = document.getElementsByClassName("agenda-button-session3");
+    color[0].style.background = "#53284f";
+
+    color = document.getElementsByClassName("agenda-button-session4");
+    color[0].style.background = "#53284f";
+
+    var test = document.getElementsByClassName("session1")
+    for (var i = 0; i<test.length; i ++){
+      test[i].style.visibility = "visible";
+    }
+
+    test = document.getElementsByClassName("session2")
+    for (var i = 0; i<test.length; i ++){
+      test[i].style.visibility = "visible";
+    }
+
+    test = document.getElementsByClassName("session3")
+    for (var i = 0; i<test.length; i ++){
+      test[i].style.visibility = "visible";
+    }
+
+    test = document.getElementsByClassName("session4")
+    for (var i = 0; i<test.length; i ++){
+      test[i].style.visibility = "visible";
+    }
+}
+function onButtonClick1(){
+    color = document.getElementsByClassName("agenda-button-all");
+    color[0].style.background = "#53284f";
+
+    color = document.getElementsByClassName("agenda-button-session1");
+    color[0].style.background = "#e98300";
+
+    color = document.getElementsByClassName("agenda-button-session2");
+    color[0].style.background = "#53284f";
+
+    color = document.getElementsByClassName("agenda-button-session3");
+    color[0].style.background = "#53284f";
+
+    color = document.getElementsByClassName("agenda-button-session4");
+    color[0].style.background = "#53284f";
+
+
+    var test = document.getElementsByClassName("session1")
+    for (var i = 0; i<test.length; i ++){
+      test[i].style.visibility = "visible";
+    }
+
+    test = document.getElementsByClassName("session2")
+    for (var i = 0; i<test.length; i ++){
+      test[i].style.visibility = "collapse";
+    }
+
+    test = document.getElementsByClassName("session3")
+    for (var i = 0; i<test.length; i ++){
+      test[i].style.visibility = "collapse";
+    }
+
+    test = document.getElementsByClassName("session4")
+    for (var i = 0; i<test.length; i ++){
+      test[i].style.visibility = "collapse";
+    }
+}
+
+function onButtonClick2(){
+
+    color = document.getElementsByClassName("agenda-button-all");
+    color[0].style.background = "#53284f";
+
+    color = document.getElementsByClassName("agenda-button-session1");
+    color[0].style.background = "#53284f";
+
+    color = document.getElementsByClassName("agenda-button-session2");
+    color[0].style.background = "#e98300";
+
+    color = document.getElementsByClassName("agenda-button-session3");
+    color[0].style.background = "#53284f";
+
+    color = document.getElementsByClassName("agenda-button-session4");
+    color[0].style.background = "#53284f";
+
+
+    var test = document.getElementsByClassName("session1")
+    for (var i = 0; i<test.length; i ++){
+      test[i].style.visibility = "collapse";
+    }
+
+    test = document.getElementsByClassName("session2")
+    for (var i = 0; i<test.length; i ++){
+      test[i].style.visibility = "visible";
+    }
+
+    test = document.getElementsByClassName("session3")
+    for (var i = 0; i<test.length; i ++){
+      test[i].style.visibility = "collapse";
+    }
+
+    test = document.getElementsByClassName("session4")
+    for (var i = 0; i<test.length; i ++){
+      test[i].style.visibility = "collapse";
+    }
+  }
+function onButtonClick3(){
+
+    color = document.getElementsByClassName("agenda-button-all");
+    color[0].style.background = "#53284f";
+
+    color = document.getElementsByClassName("agenda-button-session1");
+    color[0].style.background = "#53284f";
+
+    color = document.getElementsByClassName("agenda-button-session2");
+    color[0].style.background = "#53284f";
+
+    color = document.getElementsByClassName("agenda-button-session3");
+    color[0].style.background = "#e98300";
+
+    color = document.getElementsByClassName("agenda-button-session4");
+    color[0].style.background = "#53284f";
+
+    var test = document.getElementsByClassName("session1")
+    for (var i = 0; i<test.length; i ++){
+      test[i].style.visibility = "collapse";
+    }
+
+    test = document.getElementsByClassName("session2")
+    for (var i = 0; i<test.length; i ++){
+      test[i].style.visibility = "collapse";
+    }
+
+    test = document.getElementsByClassName("session3")
+    for (var i = 0; i<test.length; i ++){
+      test[i].style.visibility = "visible";
+    }
+
+    test = document.getElementsByClassName("session4")
+    for (var i = 0; i<test.length; i ++){
+      test[i].style.visibility = "collapse";
+    }
+}
+function onButtonClick4(){
+
+    color = document.getElementsByClassName("agenda-button-all");
+    color[0].style.background = "#53284f";
+
+    color = document.getElementsByClassName("agenda-button-session1");
+    color[0].style.background = "#53284f";
+
+    color = document.getElementsByClassName("agenda-button-session2");
+    color[0].style.background = "#53284f";
+
+    color = document.getElementsByClassName("agenda-button-session3");
+    color[0].style.background = "#53284f";
+
+    color = document.getElementsByClassName("agenda-button-session4");
+    color[0].style.background = "#e98300";
+
+    var test = document.getElementsByClassName("session1")
+    for (var i = 0; i<test.length; i ++){
+      test[i].style.visibility = "collapse";
+    }
+
+    test = document.getElementsByClassName("session2")
+    for (var i = 0; i<test.length; i ++){
+      test[i].style.visibility = "collapse";
+    }
+
+    test = document.getElementsByClassName("session3")
+    for (var i = 0; i<test.length; i ++){
+      test[i].style.visibility = "collapse";
+    }
+
+    test = document.getElementsByClassName("session4")
+    for (var i = 0; i<test.length; i ++){
+      test[i].style.visibility = "visible";
+    }
+}
